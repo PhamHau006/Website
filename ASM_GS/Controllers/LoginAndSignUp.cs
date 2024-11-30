b5225f6f40be228e2e2745c54d10f1c0d818845f
PhamHau67 <meow067pbh@gmail.com>
2024-11-30 15:23:12 +0700

    Update-Password

--- ASM_GS/Controllers/LoginAndSignUp.cs
+++ ASM_GS/Controllers/LoginAndSignUp.cs
@@ -536,7 +536,7 @@ namespace ASM_GS.Controllers
         {
             if(AuthenChangPass==false)
             {
-                return RedirectToAction("Index", "LoginAndSignUp");
+                return Redirect("https://localhost:7265/LoginAndSignUp");
             }    
             var TaiKhoan = _context.TaiKhoans.Where(tk => tk.Email==email).FirstOrDefault();
             ViewData["tk"] = TaiKhoan;
